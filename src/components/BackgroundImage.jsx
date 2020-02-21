b8817d13cc99b324659e6af0ee35ed9f2516f6c3
Student <example@example.com>
2020-02-21 15:58:14 -0800

    attempt to fix background image

--- src/components/BackgroundImage.jsx
+++ src/components/BackgroundImage.jsx
@@ -1,17 +1,17 @@
-import Background from '../assets/images/background.jpg';
-import React from 'react';
+// import Background from '../assets/images/background.jpg';
+// import React from 'react';
 
-function BackgroundImage(){
-  var style = {
-    backgroundPosition: 'center',
-    backgroundSize: 'cover',
-    backgroundRepeat: 'repeat',
-  };
-  return(
-    <div>
-      <img style={style} src={Background} alt="Background image"/>
-    </div>
-  );
-}
+// function BackgroundImage(){
+//   var style = {
+//     backgroundPosition: 'center',
+//     backgroundSize: 'cover',
+//     backgroundRepeat: 'repeat',
+//   };
+//   return(
+//     <div>
+//       <img style={style} src={Background} alt="Background image"/>
+//     </div>
+//   );
+// }
 
-export default BackgroundImage;
+// export default BackgroundImage;
